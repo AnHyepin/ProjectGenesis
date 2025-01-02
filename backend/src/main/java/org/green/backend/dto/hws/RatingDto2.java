6d7ea70d95dc83a29f15bd6a6e7284a4f4d5068f
unknown <yiok79@naver.com>
2025-01-02 21:16:55 +0900

    별점 기능 완려

---
+++ backend/src/main/java/org/green/backend/dto/hws/RatingDto2.java
@@ -0,0 +1,15 @@
+package org.green.backend.dto.hws;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatingDto2 {
+    private String companyName;
+    private String username;
+    private Double jrStar;
+}
